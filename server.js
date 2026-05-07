d86c518d69fc23b7ba3577f73cefe1d6394fa2d6
Arwad Rahal <arwad.rahal1@gmail.com>
2026-05-07 22:52:04 +0300

    handle product update upload errors

--- server.js
+++ server.js
@@ -45,7 +45,12 @@ const uploadToCloudinary = (fileBuffer, folder = "eloria-products") => {
 };
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
